696f4b81c7915a2f23cbca48cbfe04ef66983425
rb.balestrin <therealrenanbez@gmail.com>
2024-11-04 15:51:58 -0300

    example of unsafe code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-use std::io;
-fn main() {
-    let mut x: i32 = 1;
-    let mut y = 0;
-    let mut auxiliary:i32;
-    let mut n = String::new();
-    io::stdin().read_line(&mut n).expect("Falha");
-    let mut n = n.trim().parse().expect("Not a number");
-
-    'fibonatti: while y < n{
-        auxiliary = x + y;
-        y = x;
-        x = auxiliary;
-
-        println!("{}", auxiliary);
+fn read(y: bool) {
+    if y {
+        println!("y eh verdadeiro!");
     }
 }
 
+fn main() {
+    read(x);
+    let x = true;
+}
